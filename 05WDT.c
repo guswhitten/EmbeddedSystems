7f56797635cab41b3a7caa0a4a4b2a89556e5ad9
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2021-11-11 15:31:34 -0500

    Update 05WDT.c

--- 05WDT.c
+++ 05WDT.c
@@ -1,22 +1,25 @@
+/*
+We want to configure the clock to toggle the red and green LEDs every t = 256 ms.
+First, we must configure the clock. We will use sub-main clock (SMCLK) which has frequency 1 MHz.
+Since freq = 1 / period, we set the period to 0.256 and solve for desired freq (desired freq = 3.9 Hz)
+We must use clock dividers to get from 1 MHz to 3.9 Hzwdtie
+*/
+
 #include <msp430.h>
-#define RedLED BIT6
-#define GreenLED BIT0
-#define ToggleLeds (P1OUT ^= RedLED|GreenLED)
-#define Button BIT3
-void one(void)
+void main(void)
 {
-    BCSCTL2 |= DIVS_3;              //BCSCTL2 = 0x06
-    WDTCTL = WDT_MDLY_32;           //WDTCTL = 0x6910
-    IE1 |= WDTIE;                   //IE1 = 0x01
-    P1DIR = RedLED|GreenLED;        //P1DIR = 0x41      P1IN = 0x4C
-    P1OUT = RedLED;                 //P1OUT = 0x40
-    _enable_interrupts();
-    LPM1;
+    BCSCTL2 |= DIVS_3;              //SMCLK freq divided by 8 (1 MHz / 8 = 125 kHz)
+    WDTCTL = WDT_MDLY_32;           //SMCLK freq divided by 32,768 (125 kHz / 32768 = 3.81 Hz)
+    IE1 |= WDTIE;                   //To use WDT+ as an interval timer, this bit must be set to request an interrupt
+    P1DIR = BIT6|BIT0;              //sets P1DIR to 0x41, making red onboard LED (p1.6) and green onboard LED (p1.0) outputs
+    P1OUT = BIT6;                   //sets P1OUT to 0x40, so red LED starts on
+    _enable_interrupts();           //enables interrupts
+    LPM1;                           //Lower power mode --> CPU and MCLK disabled. SMCLK and ACLK still active
 }
 #pragma vector = WDT_VECTOR
 __interrupt void WDT(void)
     {
-     P1OUT = ToggleLeds;
+     P1OUT ^= BIT6|BIT0;            //XOR operation toggles LEDs once per clock cycle
     }
 
 
