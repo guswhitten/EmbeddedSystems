99a23c9e5c6bcfc772d2dd58e368c8f8a0c72eff
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2021-11-11 14:32:55 -0500

    Create TimerPWM.c

---
+++ TimerPWM.c
@@ -0,0 +1,21 @@
+#include <msp430.h>
+#define RedLED BIT6
+#define GreenLED BIT0
+#define ToggleLeds (P1OUT ^= RedLED|GreenLED)
+#define Button BIT3
+void one(void)
+{
+    BCSCTL2 |= DIVS_3;              //BCSCTL2 = 0x06
+    WDTCTL = WDT_MDLY_32;           //WDTCTL = 0x6910
+    IE1 |= WDTIE;                   //IE1 = 0x01
+    P1DIR = RedLED|GreenLED;        //P1DIR = 0x41      P1IN = 0x4C
+    P1OUT = RedLED;                 //P1OUT = 0x40
+    _enable_interrupts();
+    LPM1;
+}
+#pragma vector = WDT_VECTOR
+__interrupt void WDT(void)
+    {
+     P1OUT = ToggleLeds;
+    }
+
