405b5472d65e48bfbbb74ce0ec2bf397bbd4573b
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2021-11-11 18:18:16 -0500

    Update 05HardwarePWM.c

--- 05HardwarePWM.c
+++ 05HardwarePWM.c
@@ -1,13 +1,19 @@
+/*
+This code generates a PWM signal using hardware components and an interrupt
+
+*/
+
+
 #include "msp430G2553.h"
 void main(void)
 {
- WDTCTL = WDTPW + WDTHOLD;  // Stop WDT
+ WDTCTL = WDTPW + WDTHOLD;                   // Stop WDT
 
- P1DIR |= BIT6;             // P1.6 to output
- TACTL = TASSEL_2 + MC_1 + ID_3 + TACLR;//+TACLR;
+ P1DIR |= BIT6;                              // P1.6 to output
+ TACTL = TASSEL_2 + MC_1 + ID_3 + TACLR;     //+TACLR;
 
- TACCR0 = 31250; // Set maximum count value (PWM Period
- TACCR1 = 6250; // initialise counter compare value
+ TACCR0 = 31250;                            // Set PWM period
+ TACCR1 = 6250;                             // sets duty cycle (20%)
 
  TACCTL0 |= CCIE;
  TACCTL1 |= CCIE;
@@ -31,3 +37,6 @@ __interrupt void TA1_ISR (void)    // interrupt service routine
     TACCTL1 &=~CCIFG;
 
     }
+
+
+// result --> https://youtu.be/Vu1HVEJCDqs
